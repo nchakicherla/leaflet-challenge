a813ffdfe85df2c0eb406abbc59da2ff9c203bc3
nchakicherla <nchaki6@gmail.com>
2024-10-19 13:24:16 -0700

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,46 +17,42 @@ function getColor(depth) {
                              '#00FF00';
 }
 
-const geoJsonLayer = L.geoJSON(data, {
-    pointToLayer: function (feature, latlng) {
-        const magnitude = feature.properties.mag;
-        const depth = feature.geometry.coordinates[2];
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 
-        return L.circleMarker(latlng, {
-            radius: getRadius(magnitude),
-            fillColor: getColor(depth),
-            color: '#000',
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup(`<strong>Magnitude: ${feature.properties.mag}</strong><br>${feature.properties.place}<br>Depth: ${feature.geometry.coordinates[2]} km`);
-    }
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            const magnitude = feature.properties.mag;
+            const depth = feature.geometry.coordinates[2];
+            
+            return L.circleMarker(latlng, {
+                radius: getRadius(magnitude),
+                fillColor: getColor(depth),
+                color: '#000',
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            }).bindPopup(`<strong>Magnitude: ${feature.properties.mag}</strong><br>${feature.properties.place}<br>Depth: ${feature.geometry.coordinates[2]} km`);
+        }
+    }).addTo(map);
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+        const div = L.DomUtil.create('div', 'legend');
+        const grades = [0, 50, 100, 300, 500];  // Depth intervals
+        const colors = ['#00FF00', '#AAFF00', '#FFAA00', '#FF5500', '#FF0000'];
+
+        div.innerHTML = '<strong>Earthquake Depth (km)</strong><br>';
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + colors[i] + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + ' km<br>' : '+ km');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
+
 });
-
-geoJsonLayer.addTo(map);
-
-map.fitBounds(geoJsonLayer.getBounds());
-
-const legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-    const div = L.DomUtil.create('div', 'legend');
-    const grades = [0, 50, 100, 300, 500];  // Depth intervals
-    const colors = ['#00FF00', '#AAFF00', '#FFAA00', '#FF5500', '#FF0000'];
-
-    div.innerHTML = '<strong>Earthquake Depth (km)</strong><br>';
-
-
-    for (let i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + colors[i] + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + ' km<br>' : '+ km');
-    }
-
-    return div;
-};
-
-legend.addTo(map);
